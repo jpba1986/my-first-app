b50bdec1bd25be3780acacfe0b3a7c7e9f3ee676
Juan Pablo Barria Aguila <juan.barria@falp.org>
2019-06-26 15:22:03 -0400

    assig3 done.

--- src/app/assig3/assig3.component.ts
+++ src/app/assig3/assig3.component.ts
@@ -3,13 +3,32 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-assig3',
   templateUrl: './assig3.component.html',
-  styleUrls: ['./assig3.component.css']
+  //styleUrls: ['./assig3.component.css']
+  styles:[`
+    .displayColor{
+      color: white;
+      background-color : blue;
+  }`]
 })
 export class Assig3Component implements OnInit {
 
-  constructor() { }
+  showP = false;
+  logArray = [];
 
+  constructor() { }
   ngOnInit() {
   }
 
+  onDisplayDet(){
+    let i = 1;
+    this.showP = !this.showP;
+    this.logArray.push(i);    
+  }
+
+  /*getPColor(){
+    if (this.logArray.length >=5){
+      console.log(this.logArray.length);
+      return 'background-color: blue;';
+    }
+  }*/
 }
